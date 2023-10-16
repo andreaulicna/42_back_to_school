96a8638a9266dd02280e211472770905793eb06c
aulicna <andrea@DESKTOP-7ESJEVQ.>
2023-10-16 17:53:53 +0200

    Exam Week 01

---
+++ exam_week_01/search_and_replace/search_and_replace.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   search_and_replace.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aulicna <aulicna@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/16 17:22:53 by aulicna           #+#    #+#             */
+/*   Updated: 2023/10/16 17:52:06 by aulicna          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc == 4 && ft_strlen(argv[2]) == 1 && ft_strlen(argv[3]) == 1)
+	{
+		i = 0;
+		while (argv[1][i])
+		{
+			if (argv[1][i] == argv[2][0])
+				write(1, &argv[3][0], 1);
+			else
+				write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}
