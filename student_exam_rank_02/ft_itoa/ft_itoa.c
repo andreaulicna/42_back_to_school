1210c21620affdd77dd2ce445df80d86344bbb22
aulicna <106449030+andreaulicna@users.noreply.github.com>
2023-10-20 17:48:27 +0200

    Student Exam Rank 2 Done

--- student_exam_rank_02/ft_itoa/ft_itoa.c
+++ student_exam_rank_02/ft_itoa/ft_itoa.c
@@ -6,52 +6,52 @@
 /*   By: aulicna <aulicna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 16:44:19 by aulicna           #+#    #+#             */
-/*   Updated: 2023/10/20 17:30:20 by aulicna          ###   ########.fr       */
+/*   Updated: 2023/10/20 17:47:08 by aulicna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int ft_nbrlen(int nbr)
+int	ft_nbrlen(int nbr)
 {
-    int len;
+	int	len;
 
-    len = 0;
-    if (nbr < 0)
-        len++;
-    while (nbr != 0)
-    {
-        nbr /= 10;
-        len++;
-    }
-    return (len);
+	len = 0;
+	if (nbr < 0)
+		len++;
+	while (nbr != 0)
+	{
+		nbr /= 10;
+		len++;
+	}
+	return (len);
 }
 
-char    *ft_itoa(int nbr)
+char	*ft_itoa(int nbr)
 {
-    int     len;
-    char    *s;
+	int		len;
+	char	*s;
 
-    len = ft_nbrlen(nbr);
-    s = (char *) malloc(sizeof(char) * (len + 1));
-    if (!s)
-        return (NULL);
-    s[len] = '\0';
-    if (nbr == 0)
-        s[0] = '0';
-    else if (nbr < 0)
-        s[0] = '-';
-    len -= 1;
-    while (nbr != 0)
-    {
-        if (nbr > 0)
-			s[len] = nbr % 10 + '0';
+	len = ft_nbrlen(nbr);
+	s = (char *) malloc(sizeof(char) * (len + 1));
+	if (!s)
+		return (NULL);
+	s[len] = '\0';
+	if (nbr == 0)
+		s[0] = '0';
+	else if (nbr < 0)
+		s[0] = '-';
+	len -= 1;
+	while (nbr != 0)
+	{
+		if (nbr > 0)
+			[len] = nbr % 10 + '0';
 		else
-			s[len] = (nbr % 10) * (-1) + '0';
-		len--;
-		nbr /= 10;
+			[len] = (nbr % 10) * (-1) + '0';
+		n--;
+		r /= 10;
 	}
-    return (s);
+	return (s);
 }
 
 //int main(void)
--- student_exam_rank_02/rot_13/rot_13.c
+++ student_exam_rank_02/rot_13/rot_13.c
@@ -6,7 +6,7 @@
 /*   By: aulicna <aulicna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/19 11:17:26 by aulicna           #+#    #+#             */
-/*   Updated: 2023/10/19 11:23:39 by aulicna          ###   ########.fr       */
+/*   Updated: 2023/10/20 17:47:01 by aulicna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(int argc, char **argv)
 {
-	int	i;
+	int		i;
 	char	*str;
 
 	if (argc == 2)
@@ -25,14 +25,13 @@ int	main(int argc, char **argv)
 		{
 			if (('A' <= str[i] && str[i] <= 'M')
 				|| ('a' <= str[i] && str[i] <= 'm'))
-				str[i] += 13; 
+				str[i] += 13;
 			else if (('N' <= str[i] && str[i] <= 'Z')
 				|| ('n' <= str[i] && str[i] <= 'z'))
-				str[i] -= 13; 
+				str[i] -= 13;
 			write(1, &str[i], 1);
 			i++;
 		}
-
 	}
 	write(1, "\n", 1);
 	return (0);
